2a9405b57089f4a89bf441adc68316e15f2ac9d6
dreammaker17 <45710013+dreammaker17@users.noreply.github.com>
2021-03-24 21:07:19 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(435, 91);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(309, 243);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Draw and Show";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(49, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(356, 23);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 212);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Square:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(488, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "\"Введите координаты через запятую и пробел  (Например: 0, 0, 0, 100, 100, 100, 10" +
+    "0, 0 ).\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        private Pen _pen;
+        private int _height;
+        private int _width;
+        private int[] x;
+        private int[] y;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            _pen = new Pen(Color.Black);
+            _height = pictureBox1.Height;
+            _width = pictureBox1.Width;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+            {
+                _pen = new Pen(Color.Black);
+
+                var str = textBox1.Text.Split(", ");
+
+                var n = Decimal.ToInt32(Math.Truncate((decimal)str.Length / 2));
+
+                x = new int[n];
+                y = new int[n];
+
+                g.Clear(Color.White);
+                g.DrawLine(_pen, new Point(_width / 2, 0), new Point(_width / 2, _height));
+                g.DrawLine(_pen, new Point(0, _height / 2), new Point(_width, _height / 2));
+                _pen.Color = Color.Red;
+                int res;
+                try
+                {
+                    for (int i = 0; i < str.Length; i += 2)
+                    {
+                        x[i / 2] = int.TryParse(str[i], out res) ? res : throw new Exception();
+                    }
+
+                    for (int i = 1; i < str.Length; i += 2)
+                    {
+                        y[Decimal.ToInt32(Math.Truncate((decimal)i / 2))] = int.TryParse(str[i], out res) ? res : throw new Exception();
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    label1.Text = "Error";
+                }
+                var square = 0;
+                for (int i = 0; i < n - 1; i++)
+                {
+                    g.DrawLine(_pen, new Point(x[i] + _width / 2, _height - (y[i] + _height / 2)), new Point(x[i + 1] + _width / 2, _height - (y[i + 1] + _height / 2)));
+                    square += x[i] * y[i + 1] - y[i] * x[i + 1]; // cчитаем площадь
+                }
+
+                g.DrawLine(_pen, new Point(x[n - 1] + _width / 2, _height - (y[n - 1] + _height / 2)), new Point(x[0] + _width / 2, _height - (y[0] + _height / 2)));
+
+                square += x[n - 1] * y[0] - y[n - 1] * x[0];
+                square = Math.Abs(square / 2);
+                label1.Text = "Square = " + square;
+            }
+        }
+    }
+}
